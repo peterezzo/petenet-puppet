1f3b446ecfe981d6ec4e6c1171e56d35ddcdc22d
Pete Ezzo <peter.ezzo@gmail.com>
2016-06-18 08:51:31 -0400

    allow bootstrap puppet script to be run again in case of failure

--- support_scripts/bootstrap-puppet.sh
+++ support_scripts/bootstrap-puppet.sh
@@ -4,20 +4,25 @@
 # it does a hacky detection based on distro /etc files but works so far
 # it also assumes that there will not be collisions of required files in /etc/puppet
 
-if [ -e /etc/redhat-release ]; then
-    echo "Red Hat detected"
-    # only support centos7 for now
-    rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-    yes | yum -y install puppet
-elif [ -e /etc/debian_version ]; then
-    echo "Debian detected"
-    # only support 14.04 for now
-    wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-    dpkg -i /puppetlabs-release-trusty.deb
-    apt-get -y update
-    apt-get -y install puppet
+if [ `which puppet` ]' then:
+    # maybe do some version checking here?
+    echo "Skipping puppet reinstall"
 else
-    echo "Unsupported release detected"
+    if [ -e /etc/redhat-release ]; then
+        echo "Red Hat detected"
+        # only support centos7 for now
+        rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+        yes | yum -y install puppet
+    elif [ -e /etc/debian_version ]; then
+        echo "Debian detected"
+        # only support 14.04 for now
+        wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+        dpkg -i /puppetlabs-release-trusty.deb
+        apt-get -y update
+        apt-get -y install puppet
+    else
+        echo "Unsupported release detected"
+    fi
 fi
 
 if [ `which puppet` ]; then

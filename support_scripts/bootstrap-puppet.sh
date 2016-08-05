ce7648aa9b1affdc4afea9f405523cdf065af2b7
Pete Ezzo <peter.ezzo@gmail.com>
2016-08-05 15:34:58 -0400

    initial shift to librarian in bootstrap script

--- support_scripts/bootstrap-puppet.sh
+++ support_scripts/bootstrap-puppet.sh
@@ -4,39 +4,42 @@
 # it does a hacky detection based on distro /etc files but works so far
 # it also assumes that there will not be collisions of required files in /etc/puppet
 
-if [ `which puppet` ]; then
-    # maybe do some version checking here?
-    echo "Skipping puppet reinstall"
+PUPPETDIR=/etc/puppet
+
+if [ -e /etc/redhat-release ]; then
+    echo "Red Hat detected"
+    # only support centos7 for now
+    rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+    yes | yum -y install puppet
+elif [ -e /etc/debian_version ]; then
+    echo "Debian detected"
+    # only support 14.04 for now
+    apt-get -y install wget
+    # TODO: this needs a lot more robust verification that download happened!
+    wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+    dpkg -i /puppetlabs-release-trusty.deb
+    apt-get -y update
+    apt-get -y install puppet
 else
-    if [ -e /etc/redhat-release ]; then
-        echo "Red Hat detected"
-        # only support centos7 for now
-        rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-        yes | yum -y install puppet
-    elif [ -e /etc/debian_version ]; then
-        echo "Debian detected"
-        # only support 14.04 for now
-        apt-get -y install wget
-        # TODO: this needs a lot more robust verification that download happened!
-        wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-        dpkg -i /puppetlabs-release-trusty.deb
-        apt-get -y update
-        apt-get -y install puppet
-    else
-        echo "Unsupported release detected"
-    fi
+    echo "Unsupported release detected"
+    exit 1
+fi
+
+gem install librarian-puppet
+if [ `which librarian-puppet` ]; then
+    cd ${PUPPETDIR}
+    librarian-puppet update
+else
+    echo "Could not setup librarian-puppet"
+    exit 1
 fi
 
 if [ `which puppet` ]; then
-    puppet module install puppetlabs-stdlib
-    puppet module install puppetlabs-apt
-    puppet module install puppetlabs-git
-    puppet module install puppetlabs-ntp
-    puppet module install herculesteam-augeasproviders_ssh
-    puppet module install herculesteam-augeasproviders_shellvar
-    puppet module install herculesteam-augeasproviders_pam
-    puppet module install herculesteam-augeasproviders_puppet
-    puppet apply /etc/puppet/manifests/site.pp
+    # do this a few times to apply consistent state, can we detect errors?
+    cd ${PUPPETDIR}
+    puppet apply manifests/site.pp
+    puppet apply manifests/site.pp
+    puppet apply manifests/site.pp
 else
     echo "Could not setup puppet"
     exit 1
